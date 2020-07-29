fb89717bad3af7cd09f572d88fa434f455ed1263
Aymeric Toulouse <aymeric.toulouse@gmail.com>
2020-07-29 13:05:26 +0200

    wed29jul - more explaination about my mrt job

--- src/components/Refs/Refs.js
+++ src/components/Refs/Refs.js
@@ -9,13 +9,13 @@ import {
   DialogContent,
   DialogTitle,
   Box,
-  useMediaQuery,
+  useMediaQuery, ListItemIcon,
 } from '@material-ui/core';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
-import { MoreHoriz, Close } from '@material-ui/icons';
+import { MoreHoriz, Close, Launch } from '@material-ui/icons';
 
 import styles from './Refs.scss';
 
@@ -122,14 +122,56 @@ const Refs = () => {
         </div>
 
         <div className={styles.containContainer}>
-          <h4>Full Stack Developer</h4>
+          <h4>Lead Developer</h4>
           <p>
-            Mars 2018 - summer 2020 | 2 ½ years | Paris - Bordeaux
+            January 2020 - August 2020 | 8 months | Bordeaux
             <br />
             <hr />
-            Refonte du SI/ERP pour du custom (sites web + automatisation de l'atelier de production)
+            Management and recruitment
+          </p>
+          <h4>FullStack Developer Javascript - Software Engineer</h4>
+          <p>
+            Mars 2018 - January 2020 | 1,6 years | Paris - Bordeaux
             <br />
-            3 sites PWA, 1 backoffice, 1 app mobile, 1 app desktop, 1 app raspberry, 2 apis, 1 database
+            <hr />
+            Refonte de tout le SI/ERP pour du custom:
+            <br />
+            3 sites PWA en ReactJs pour les clients
+            <br />
+            Automatisation de l'atelier de production de 300m² à Bordeaux:
+            <br />
+            - pour les metiers: 1 backoffice ReactJs
+            <br />
+            - pour les opérateurs: app hybride mobile React-Native sur 6 devices, 1 app desktop Electron sur 15 Mac,
+            1 app Electron sur 60 Raspberry Pi
+            <br />
+            Servi par 2 apis NodeJs/GraphQl, 1 database PostgreSQL et des serveurs en local pour des besoins
+            spécifiques type imprimante, pick2light, ligne d'expédition
+            <br />
+            Orchestré et scalé en cloud sur AWS
+            <br />
+            <br />
+
+            <ListItem
+              button
+              className={styles.listItem}
+              component="a"
+              target={'_blank'}
+              href={'https://monsieurtshirt.com'}
+            >
+              <ListItemIcon><Launch /></ListItemIcon>
+              <ListItemText primary={'le front: monsieurtshirt.com'} />
+            </ListItem>
+            <ListItem
+              button
+              className={styles.listItem}
+              component="a"
+              target={'_blank'}
+              href={'https://youtu.be/4I5w0CrtnyE'}
+            >
+              <ListItemIcon><Launch /></ListItemIcon>
+              <ListItemText primary={'visite de l\'atelier à Bordeaux'} />
+            </ListItem>
           </p>
 
           <Button
