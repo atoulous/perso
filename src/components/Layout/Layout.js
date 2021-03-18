d9afb498325a4583eb5ebaf12dd03a02ffe805a0
Aymeric Toulouse <rico@MacBook-Pro-de-Aymeric.local>
2021-03-18 17:26:18 +0100

    thu18mar fix background preload timing and add disable code loader

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -9,6 +9,7 @@ import {
 import {
   Grade, Work as WorkIcon, Person, Home as HomeIcon, Devices, ArrowDropUp,
 } from '@material-ui/icons';
+import Loader from '../Loader';
 
 import { darkTheme, defaultTheme } from '../../themes';
 import { ThemeContext } from '../../providers/ThemeContext';
@@ -39,60 +40,84 @@ const Layout = ({ children }) => {
     setBgLoad(true);
   };
 
+  // if (!bgLoad) {
+  //   return (
+  //     <>
+  //       <Loader styles={styles.loader} />
+  //       <img
+  //         onLoad={bgIndex && handleBgLoad}
+  //         src={bgs[bgIndex]}
+  //         key={bgs[bgIndex]}
+  //         className={cc(styles.background, !bgLoad && styles.bgHidden)}
+  //         alt="bg"
+  //       />
+  //     </>
+  //   );
+  // }
+
   return (
-    <ThemeProvider theme={themeObj}>
+    <>
+      <ThemeProvider theme={themeObj}>
+        {
+          !bgLoad && <div className={cc(styles.background, styles.lazyBg)} />
+        }
+        <img
+          onLoad={bgIndex && handleBgLoad}
+          src={bgs[bgIndex]}
+          key={bgs[bgIndex]}
+          className={cc(styles.background, !bgLoad && styles.bgHidden)}
+          alt="bg"
+        />
+
+        <div className={cc(styles.layout)}>
+          <header className={styles.header}>
+            <ButtonGroup variant="text" className={styles.buttonGroup} aria-label="text primary button group">
+              <LinkScroll to={'home'} smooth>
+                <Button startIcon={matchWidth && <HomeIcon />} className={matchWidth && styles.button}>Home</Button>
+              </LinkScroll>
+              <LinkScroll to={'skills'} smooth>
+                <Button startIcon={matchWidth && <Grade />} className={matchWidth && styles.button}>Skills</Button>
+              </LinkScroll>
+              <LinkScroll to={'works'} smooth>
+                <Button startIcon={matchWidth && <Devices />} className={matchWidth && styles.button}>Works</Button>
+              </LinkScroll>
+              <LinkScroll to={'refs'} smooth>
+                <Button startIcon={matchWidth && <WorkIcon />} className={matchWidth && styles.button}>Refs</Button>
+              </LinkScroll>
+              <LinkScroll to={'contact'} smooth>
+                <Button startIcon={matchWidth && <Person />} className={matchWidth && styles.button}>Contact</Button>
+              </LinkScroll>
+            </ButtonGroup>
+
+            <Slider
+              hour={hour}
+              setHour={setHour}
+            />
+          </header>
+          <Container>
+            { children }
+          </Container>
+          <footer className={styles.footer}>
+            <p>Good to know, I currently work on mac with terminal/vim and my favorite IDE is jetbrains Intellij IDEA.</p>
+            <LinkScroll to={'home'} smooth>
+              <Avatar className={styles.avatar}>
+                <ArrowDropUp className={styles.icon} />
+              </Avatar>
+            </LinkScroll>
+          </footer>
+        </div>
+      </ThemeProvider>
       {
-        !bgLoad && <div className={cc(styles.background, styles.lazyBg)} />
-      }
-      {
-        bgs.map((bg, index) => bg && (
+        bgLoad && bgs.map((bg, index) => bg && bgIndex !== index && (
           <img
-            onLoad={bgIndex === index && handleBgLoad}
             src={bg}
             key={bg}
-            className={cc(styles.background, (!bgLoad || bgIndex !== index) && styles.bgHidden)}
+            className={cc(styles.background, styles.bgHidden)}
             alt="bg"
           />
         ))
       }
-      <div className={cc(styles.layout)}>
-        <header className={styles.header}>
-          <ButtonGroup variant="text" className={styles.buttonGroup} aria-label="text primary button group">
-            <LinkScroll to={'home'} smooth>
-              <Button startIcon={matchWidth && <HomeIcon />} className={matchWidth && styles.button}>Home</Button>
-            </LinkScroll>
-            <LinkScroll to={'skills'} smooth>
-              <Button startIcon={matchWidth && <Grade />} className={matchWidth && styles.button}>Skills</Button>
-            </LinkScroll>
-            <LinkScroll to={'works'} smooth>
-              <Button startIcon={matchWidth && <Devices />} className={matchWidth && styles.button}>Works</Button>
-            </LinkScroll>
-            <LinkScroll to={'refs'} smooth>
-              <Button startIcon={matchWidth && <WorkIcon />} className={matchWidth && styles.button}>Refs</Button>
-            </LinkScroll>
-            <LinkScroll to={'contact'} smooth>
-              <Button startIcon={matchWidth && <Person />} className={matchWidth && styles.button}>Contact</Button>
-            </LinkScroll>
-          </ButtonGroup>
-
-          <Slider
-            hour={hour}
-            setHour={setHour}
-          />
-        </header>
-        <Container>
-          { children }
-        </Container>
-        <footer className={styles.footer}>
-          <p>Good to know, I currently work on mac with terminal/vim and my favorite IDE is jetbrains Intellij IDEA.</p>
-          <LinkScroll to={'home'} smooth>
-            <Avatar className={styles.avatar}>
-              <ArrowDropUp className={styles.icon} />
-            </Avatar>
-          </LinkScroll>
-        </footer>
-      </div>
-    </ThemeProvider>
+    </>
   );
 };
 
